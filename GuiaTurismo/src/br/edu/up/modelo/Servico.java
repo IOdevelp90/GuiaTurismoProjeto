1736daa1cc6d6dec368fce73a31696c806ca9317
IOdevelp90 <emanuelcgalindo@hotmail.com>
2024-06-07 10:44:28 -0300

    manuatualizao

--- GuiaTurismo/src/br/edu/up/modelo/Servico.java
+++ GuiaTurismo/src/br/edu/up/modelo/Servico.java
@@ -1,5 +1,24 @@
 package br.edu.up.modelo;
 
+import java.util.List;
+
 public class Servico {
+    private int id;
+    private String nome;
+    private String descricao;
+    private double preco;
+
+    public servico(int id, String nome, String descricao, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco; 
+    }
+
+    public int getID() {
+        return id;
+}
+
+    
 
 }
