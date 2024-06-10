ed677c694288f56ad8e6076ab90f156052e0482a
IOdevelp90 <emanuelcgalindo@hotmail.com>
2024-06-10 10:28:27 -0300

    Controle Serviço manu

--- GuiaTurismo/src/br/edu/up/controle/ControleDeServicos.java
+++ GuiaTurismo/src/br/edu/up/controle/ControleDeServicos.java
@@ -1,5 +1,25 @@
 package br.edu.up.controle;
 
+    import java.util.ArrayList;
+    import java.util.List;
+    import br.edu.up.modelo.Servico;
+    
 public class ControleDeServicos {
 
+      List<Servico>servicos = new ArrayList<>();
+
+      public ControleDeServicos(List<Servico> servicos) {
+        this.servicos = servicos;
+    
+      }
+    
+      public void adicionarServicos(){
+        Servico servico = new Servico(null, null, 0);
+        servicos.add(servico);
+      }
+      // atualizacoes
+    
 }
+    
+    
+
